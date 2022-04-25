8d73b2df62a2535dafcb3e0e204949218d7eacc8
nelix8 <104223593893+nelix8@users.noreply.github.com>
2022-04-25 20:49:06 +0100

    Testing Framework

---
+++ core/src/tests/FormatTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FormatTest {
+
+	@Test
+	void test() {
+		assertEquals(1,1);
+	}
+
+}
