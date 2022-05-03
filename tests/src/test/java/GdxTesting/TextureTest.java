8384245702fd445585aa3c78b6df6dfdda887b14
nelix8 <104223893+nelix8@users.noreply.github.com>
2022-05-03 12:01:42 +0100

    Asset test update

--- tests/src/test/java/GdxTesting/TextureTest.java
+++ tests/src/test/java/GdxTesting/TextureTest.java
@@ -10,8 +10,12 @@ import com.badlogic.gdx.Gdx;
 import test.java.GdxTesting.GdxTestRunner;
 
 @RunWith(GdxTestRunner.class)
-public class TextureTest {
-
+public class AssetTest {
+	
+	/*Tests are standardised here, the code is to check that all textures are accessible and
+	  present categorised by their place in the game.
+	 */
+	
 	@Test
 	public void shipTexturesExist() {
 		String[] ships = { "ship1", "ship2", "ship3"};
@@ -21,8 +25,50 @@ public class TextureTest {
 	}
 	
 	@Test
-	public void basicTest() {
-		assertTrue(true);
+	public void powerupTexturesExist() {
+		String[] ships = { "Shield", "Freeze", "Range", "DamageUp", "SpeedUp"};
+		for (String s : ships) {
+			assertTrue("the file " + s + ".png does not exist", Gdx.files.internal("../core/assets/models/" + s + ".png").exists());
+		}
 	}
-
+	
+	@Test
+	public void obstacleTexturesExist() {
+		String[] ships = {"BarrelFloat"};
+		for (String s : ships) {
+			assertTrue("the file " + s + ".png does not exist", Gdx.files.internal("../core/assets/models/" + s + ".png").exists());
+		}
+	}
+	
+	@Test
+	public void cannonballTexturesExist() {
+		String[] ships = {"cannonBall"};
+		for (String s : ships) {
+			assertTrue("the file " + s + ".png does not exist", Gdx.files.internal("../core/assets/models/" + s + ".png").exists());
+		}
+	}
+	
+	@Test
+	public void statusIconsTexturesExist() {
+		String[] ships = {"gold_coin", "burst_icon", "attack_icon", "HealthUp", "Repair", "burst_onCoolDown", "bar", "ShieldBar"};
+		for (String s : ships) {
+			assertTrue("the file " + s + ".png does not exist", Gdx.files.internal("../core/assets/models/" + s + ".png").exists());
+		}
+	}
+	
+	@Test
+	public void mapTexturesExist() {
+		String[] ships = {"background.png", "castle.png", "water.jpg"};
+		for (String s : ships) {
+			assertTrue("the file " + s + ".png does not exist", Gdx.files.internal("../core/assets/models/" + s).exists());
+		}
+	}
+	
+	@Test
+	public void audioFilesExist() {
+		String[] ships = {"music", "seaSounds", "cannonShot", "buttonPress", "explosion"};
+		for (String s : ships) {
+			assertTrue("the file " + s + ".mp3 does not exist", Gdx.files.internal("../core/assets/models/" + s + ".mp3").exists());
+		}
+	}
 }
